9c3b2f32634c853fd6c1932711b50b9a0d43b593
Kharacha <61607339+Kharacha@users.noreply.github.com>
2023-10-28 13:56:58 -0700

    made navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+// import logo from './logo.svg'; 
+import React from 'react';
+import Navbar from './Navbar'; // Import the Navbar component
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 function App() {
   return (
+    <router> 
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> hey
+           <code> </code> 
         </p>
-        <a
+        <div
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          <Navbar />
+        </div>
       </header>
     </div>
+      `<switch>
+        <route path="/" component={Home}/>
+        <route path="/" component={Home}/>
+        <route path="/" component={Home}/>
+        <route path="/" component={Home}/>
+        <route path="/" component={Home}/>
+      </switch>`
+    </router>
+  );
+  
+}
+
+export default App;
+
+
+import React from 'react';
+
+
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
