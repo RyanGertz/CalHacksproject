137e3c64db5bc7c1d454e29f3dce090d722fa860
Ryan Gertz <ryan.gertz1@gmail.com>
2023-10-28 23:29:56 -0700

    weird bugs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Signup from './Signup'
 import Login from './Login'
+import Creators from './creators'
 
 
 
@@ -19,6 +20,7 @@ function App() {
           <Route path="/" activeClassName="active" element={ <Home /> }/>
           <Route path="/signup" activeClassName="active" element={ <Signup /> }/>
           <Route path="/login" activeClassName="active" element={ <Login /> }/>
+          <Route path="/creators" activeClassName="active" element={ <Creators /> }/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,18 +7,11 @@ function Navbar() {
     <div className="navbar">
         <nav>
             <ul>
-<<<<<<< Updated upstream
                 <li><NavLink to="/"  end>Home</NavLink></li>
                 <li><NavLink to="/creators">Creators</NavLink></li>
                 <li><NavLink to="/nfts">NFTs</NavLink></li>
                 <li><NavLink to="/events">My Events</NavLink></li>
                 <li><NavLink to="/account">Login/Sign-up</NavLink></li>
-=======
-                <li><a href="/" className="Home"> Home </a></li>
-                <li><a href="/creators"> Creators </a></li>
-                <li><a href="/events"> My Events </a></li>
-                <li><a href="/account"> Login/Sign-up </a></li>
->>>>>>> Stashed changes
             </ul>
         </nav>
     </div>
